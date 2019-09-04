5e9c748f738a68c815904f1c173495edc32fee18
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-09-04 11:02:11 +0200

    Start project

---
+++ src/test/java/fr/canal/plus/PlusApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.canal.plus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ src/test/java/fr/canal/plus/cucumber/ClientServiceSteps.java
@@ -0,0 +1,31 @@
+package fr.canal.plus.cucumber;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+public class ClientServiceSteps {
+
+    @Given("^a client with a main address active in France$")
+    public void a_client_with_a_main_address_active_in_France() throws Throwable {
+    }
+
+    @When("^the advisor connected to canal changes the client address without an effective date$")
+    public void the_advisor_connected_to_canal_changes_the_client_address_without_an_effective_date() throws Throwable {
+        Assert.assertFalse(true);
+    }
+
+    @Then("^the modified client address is recorded on all subscriber contracts$")
+    public void the_modified_client_address_is_recorded_on_all_subscriber_contracts() throws Throwable {
+        Assert.assertFalse(true);
+
+    }
+
+    @And("^an address change movement is created$")
+    public void an_address_change_movement_is_created() {
+        Assert.assertFalse(true);
+    }
+
+}
